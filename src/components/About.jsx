7525b7391b00c12cd6e497327ef13c94c458f224
DraconPern <draconpern@hotmail.com>
2019-12-16 05:44:38 -0600

    add about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const About = () => (
   <div>
     <p>A tool for espressomedical that works on a study.  You can change the name and or ID.  You can delete a study.</p>
+    <div><a href="/">Click here to start using the tool</a></div>
   </div>
 );
 
